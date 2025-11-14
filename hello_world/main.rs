2928e5f382b72424a33143be1c790847582a32ea
Syed <syedahmedd.02@gmail.com>
2025-11-14 00:48:27 -0500

    hello world

---
+++ hello_world/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // ! defines the function as a rust macro, if it had called a func instead it would be defined as println
+    println!("Hello World");
+}
+
+// rustc main.rs compiled the code
