dd13ada65c02be2bf64a4bfc05598d69823cfd7d
Syed <syedahmedd.02@gmail.com>
2025-11-30 23:26:37 -0500

    comopleted control flow

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -50,7 +50,7 @@
 
 // blocks of code evaluvate to the last expression in them, numbers by themselves are also expresions
 // . In this case, the value of the whole if expression depends on which block of code executes. This means the values that have the potential to be results from each arm of the if must be the same type
-// if the types are mismatched we get an error 
+// // if the types are mismatched we get an error 
 // fn main() {
 //     let condition = true;
 
@@ -58,3 +58,119 @@
 
 //     println!("The value of number is: {number}");
 // }
+
+
+// Repitition with loops
+// Rust provides serveral ways to execute a block of code more than once
+
+// Rust has three kinds of loops; loop, while and for
+
+//Repeating code with Loop
+// The loop keyword tells rust to excute a code block forever or untill you tell it to stop
+
+//Example
+// fn main() {
+//     loop {
+//         println!("again!");
+//     }
+// }
+
+// This code will run forever untill we stop the program manually using ctrl + c
+//  You can also break out the code using the "break" keyword within the loop to tell the program to stop executing the loop
+
+//Returning values from loops
+// one of the uses of loop is to retry an operation you know might fail such as checking wheather a thread has completed its jobs
+// You might need to pass the result, to do this add the value you want returned after the break statement
+
+//Example
+// fn main() {
+//     let mut counter = 0;
+
+//     let result = loop {
+//         counter += 1;
+
+//         if counter == 10 {
+//             break counter * 2;
+//         }
+//     };
+
+//     println!("The result is {result}");
+// }
+
+// You can aslo "return" from inside the loop, while break only exits the current loop, return always exits the current function
+
+// Loop Labels to Disambiguate Between Multiple Loops
+// If you have loops within loops, break and continue apply to the innermost loop at that point
+// You can optionally specify a loop label that you can use with break or continue 
+
+//Example
+// fn main() {
+//     let mut count = 0;
+//     'counting_up: loop {
+//         println!("count = {count}");
+//         let mut remaining = 10;
+
+//         loop {
+//             println!("remaining = {remaining}");
+//             if remaining == 9 {
+//                 break;
+//             }
+//             if count == 2 {
+//                 break 'counting_up;
+//             }
+//             remaining -= 1;
+//         }
+
+//         count += 1;
+//     }
+//     println!("End count = {count}");
+// }
+
+// The output 
+// count = 0
+// remaining = 10
+// remaining = 9
+// count = 1
+// remaining = 10
+// remaining = 9
+// count = 2
+// remaining = 10
+// End count = 2
+
+//Conditional Loops with While
+// We use conditional loops when we want to repeat code when a condition is true 
+//Example
+// fn main() {
+//     let mut number = 3;
+
+//     while number != 0 {
+//         println!("{number}!");
+
+//         number -= 1;
+//     }
+
+//     println!("LIFTOFF!!!");
+// }
+
+//Looping through a collection with for
+// Lets say you want to loop through the elements in an array, you can use the for keyword to do it
+// fn main() {
+//     let a = [10, 20, 30, 40, 50];
+
+//     for element in a {
+//         println!("the value is: {element}");
+//     }
+// }
+
+// This code is more safe then the while loop version as it eliminated the chance of trying to index a value that doesn't exist in an array
+
+// Even in situations you want to run some code a certain amount of times such as in the countdown example for while loop
+// YOu can use a floop loop using the Range provided by the standard library
+
+fn main() {
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
+// .rev reverses the range
