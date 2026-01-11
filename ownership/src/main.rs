4d65b0cdf1a4691ddd3a3c294716294a1d886d36
Syed-Ahmed02 <syedahmedd.02@gmail.com>
2026-01-10 19:30:34 -0500

    started ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -14,7 +14,29 @@
 // All data stored on the stack must have a known fixed size, data with an unknown size at compile time or size that might change must be stored on the heap
 
 //Heap
+// The heap is less organized, when you request a certain amount of space. The memory allocator finds an empty spot in the heap that is big enough
+// and marks it as being used. This is called allocating on the heap
+// accessing data on the heap is generally slower than accessing data on the stack because you have to follow a pointer to get there
 
-fn main() {
-    println!("Hello, world!");
-}
+// Keeping track of what parts of code are using what data on the heap, minimizing the amount of duplicate data and cleaning up unnused data on the heap are all problems ownership addresses
+
+
+// Ownership Rules
+
+// Each value in Rust has an owner.
+// There can only be one owner at a time.
+// When the owner goes out of scope, the value will be dropped.
+
+// Variable Scope
+// The scope refers to the range within your program for which the value is valid
+// In the following example you can see the scope of s
+
+// fn main()     {                      // s is not valid here, since it's not yet declared
+// let s = "hello";   // s is valid from this point forward
+
+// // do stuff with s
+// }                      // this scope is now over, and s is no longer valid
+
+// The string type
+
+// 
