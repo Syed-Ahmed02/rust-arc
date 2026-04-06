e599844a4d971f82df485575e7ce1895c51734bb
Syed-Ahmed02 <syedahmedd.02@gmail.com>
2026-04-06 09:19:16 -0400

    completed structs

--- rustlings/exercises/07_structs/structs2.rs
+++ rustlings/exercises/07_structs/structs2.rs
@@ -23,6 +23,7 @@ fn create_order_template() -> Order {
 
 fn main() {
     // You can optionally experiment here.
+    
 }
 
 #[cfg(test)]
@@ -32,9 +33,12 @@ mod tests {
     #[test]
     fn your_order() {
         let order_template = create_order_template();
-
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = Order {
+            name: String::from("Hacker in Rust"),
+            count: 1,
+            ..order_template
+        };
 
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
